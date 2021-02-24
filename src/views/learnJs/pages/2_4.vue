795a7cb9ba6112298a73ca13ce8bcf2a4e7f80c8
lishutao <liszter@qq.com>
2021-02-24 14:27:17 +0800

    text js

--- src/views/learnJs/pages/2_4.vue
+++ src/views/learnJs/pages/2_4.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="main-page">
-    <page-fram title="max和min">
-     <el-button @click="getMaxAndMin">ArrayMaxAndMin</el-button>
+    <page-fram title="算法测试">
+      <el-button @click="getMaxAndMin">arrMaxAndMin</el-button>
+      <el-button @click="handleFun(isPalindrome)">isABCCBA</el-button>
+      <el-button @click="replacement('11112222200001123', '1', '0')">replacement(1, 0)</el-button>
     </page-fram>
   </div>
 </template>
@@ -9,53 +11,77 @@
 import pageFram from "@/components/common/page-fram.vue";
 export default {
   components: {
-    pageFram
-   },
-  data () {
-    return {
-    };
+    pageFram,
   },
- created () {
- },
+  data() {
+    return {};
+  },
+  created() {},
   methods: {
-    getMaxAndMin () {
-      console.log(Array);
-      
-      Array.prototype.max = function() {
-        let array = this
-        if (array.length === 0) return
+    getMaxAndMin() {
+      console.log(arr);
 
-        let max = array[0]
-        for (let i = 0; i < array.length; i++) {
-          if (max < array[i]) {
-            max = array[i]
+      arr.prototype.max = function () {
+        let arr = this;
+        if (arr.length === 0) return;
+
+        let max = arr[0];
+        for (let i = 0; i < arr.length; i++) {
+          if (max < arr[i]) {
+            max = arr[i];
           }
         }
-        return max
-      }
-     Array.prototype.min = function() {
+        return max;
+      };
+      arr.prototype.min = function () {
         // this
-        let array = this
-        if (array.length === 0) return
+        let arr = this;
+        if (arr.length === 0) return;
 
-        let min = array[0]
-        for (let i = 0; i < array.length; i++) {
-          if (min > array[i]) {
-            min = array[i]
+        let min = arr[0];
+        for (let i = 0; i < arr.length; i++) {
+          if (min > arr[i]) {
+            min = arr[i];
           }
         }
-        return min
-      }
+        return min;
+      };
 
-      let a = [1, 2,3,444,5,1,2,0.2]
-      console.log(a.max() + '-----------' + a.min())
+      let a = [1, 2, 3, 444, 5, 1, 2, 0.2];
+      console.log(a.max() + "-----------" + a.min());
+    },
 
-      
+    handleFun(callback) {
+      console.log(callback());
+    },
+    isPalindrome(str = "121") {
+      // 1 去除非字母数字
+      let reg = /[^a-zA-Z0-9]$/g;
+      let arr = str.replace(reg, "").toLowerCase();
+      return arr === arr.split("").reverse().join("");
+    },
 
+    replacement(str, replace1, replace2) {
+      /**
+       * str 字符串
+       * replace1 需更换的第一个 str1
+       * replace2 需更换的第二个 str2
+       * @return 字符串
+       * **/
+
+        let arr = str.split("")
+        for (let i = 0; i < arr.length; i++) {
+          const element = arr[i]
+          if (arr[i] == replace2) {
+            arr[i] = replace1
+          } else if (arr[i] == replace1)  {
+            arr[i] = replace2
+          }
+        }
+        return arr.join('')
     }
-  }
-}
-
+  },
+};
 </script>
 <style lang='less' scoped>
 
